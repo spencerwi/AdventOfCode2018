62a0078e90c0e4a32c99e83d3dc93a9ea88e79a3
spencerwi <spencerwi@gmail.com>
2018-12-05 10:06:31 -0500

    Fix weird indentation

--- day5/day5.cr
+++ day5/day5.cr
@@ -10,37 +10,37 @@ class Day5
     def initialize(@input : String)
     end
 
-	# Part A problem statement: Find the length of the input when fully reacted.
+    # Part A problem statement: Find the length of the input when fully reacted.
     def part_a : Int32
-		react_fully(@input)
+        react_fully(@input)
     end
 
-	# Part B problem statement: Find the shortest length attainable by removing
-	# all occurences of a single letter (uppercase or lowercase) and then fully
-	# reacting the result.
-	def part_b : Int32
-		('A'..'Z').min_of do |letter| 
-			without_letter = @input.gsub(letter.upcase, "").gsub(letter.downcase, "")
-			react_fully(without_letter)
-		end
-	end
+    # Part B problem statement: Find the shortest length attainable by removing
+    # all occurences of a single letter (uppercase or lowercase) and then fully
+    # reacting the result.
+    def part_b : Int32
+        ('A'..'Z').min_of do |letter| 
+            without_letter = @input.gsub(letter.upcase, "").gsub(letter.downcase, "")
+            react_fully(without_letter)
+        end
+    end
 
-	private def react_fully(word_to_reduce)
+    private def react_fully(word_to_reduce)
         result, reactions_occurred = self.run_reactions(word_to_reduce)
         while reactions_occurred # stop once we've hit equilibrium
-			result, reaction_count = self.run_reactions(result)
+            result, reaction_count = self.run_reactions(result)
         end
         return result.size
-	end
+    end
 
     private def run_reactions(word : String) : Tuple(String, Bool)
-		# For every letter in the alphabet, remove all reactive pairs (adjacent 
-		# uppercase+lowercase and lowercase+uppercase
-		new_word = ('A'..'Z').map(&.to_s).reduce(word) do |word, char|
-			word.gsub(char + char.downcase, "")
-				.gsub(char.downcase + char, "")
-		end
-		reactions_occurred = (word.size != new_word.size)
+        # For every letter in the alphabet, remove all reactive pairs (adjacent 
+        # uppercase+lowercase and lowercase+uppercase
+        new_word = ('A'..'Z').map(&.to_s).reduce(word) do |word, char|
+            word.gsub(char + char.downcase, "")
+                .gsub(char.downcase + char, "")
+        end
+        reactions_occurred = (word.size != new_word.size)
         return {new_word, reactions_occurred}
     end
 end
