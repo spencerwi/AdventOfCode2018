ab002b6b2bfee5fffd3834254a76c46d9b34b5b9
spencerwi <spencerwi@gmail.com>
2018-12-08 07:32:20 -0500

    Factor out offset tracking into a cursor class

--- day8/day8.cr
+++ day8/day8.cr
@@ -1,3 +1,21 @@
+# A "cursor" class to keep track of where we are in the input. This structure
+# is basically what some parser-combinator libs (like OCaml's Angstrom or 
+# Haskell's Parsec) call the "state". In angstrom, this is (int * 'a) state
+class ParseCursor
+    def initialize(@input : Array(Int32), @offset : Int32 = 0)
+    end
+
+    def has_next? : Bool
+        @offset < @input.size
+    end
+
+    def next : Int32
+        current = @input[@offset]
+        @offset += 1
+        return current
+    end
+end
+
 class Node
     def initialize(@children : Array(Node), @metadata : Array(Int32))
     end
@@ -28,34 +46,37 @@ class Node
     # position scanning through the input array, so we make that part private
     # and expose a "friendlier" API for parsing a node.
     def self.parse(input : Array(Int32))
-        _, node = Node.parse_helper(input, 0)
+        node = Node.parse_helper(ParseCursor.new(input, 0))
         return node
     end
 
     # This is the ugly part: recursive parsing, where we continually pass 
-    # through our current offset in the array. 
+    # through our "cursor" into the input array. 
+    #
     # Basically, you advance through the input *strictly forward*, pulling off 
     # the child count and metadata count (since you know those will always be 
     # present), then parsing each of the children recursively, then pull off 
     # what's left as the metadata entries. 
     # 
-    # This method both accepts and returns the current offset, since it's 
-    # called recursively to parse children, so that we can tell each "child 
-    # call" where to start, and report back to the "parent call" where to 
-    # resume. That's more or less how parser-combinator libraries like OCaml's 
-    # Angstrom (or more famously Haskell's Parsec) work.    
+    # We pass through the same instance of the ParseCursor class (which is
+    # just {current_index, input} in pass-by-reference form) to keep track of
+    # our "scan position" when we call this same method recursively to parse 
+    # children, so that we can tell each "child call" where to start and know
+    # where to resume back in the "parent call". That's more or less how 
+    # parser-combinator libraries like OCaml's Angstrom (or more famously 
+    # Haskell's Parsec) work.    
     # 
     # Originally I thought about bringing in a parser-combinator lib, but then
-    # I figured I'd rather keep "lightweight" by avoiding dependencies.
-    protected def self.parse_helper(input : Array(Int32), offset) : Tuple(Int32, Node?)
-        return {offset, nil} unless offset < input.size
+    # I figured I'd rather keep "lightweight" by avoiding dependencies. So I 
+    # just read up on how Angstrom works and read a Crystal port of Parsec and 
+    # went from there.
+    protected def self.parse_helper(cursor : ParseCursor) : Node?
+        return nil unless cursor.has_next? # handle end-of-input gracefully
 
         # We're always going to have children_count and metadata_count, so grab
         # those first.
-        children_count = input[offset]
-        offset += 1
-        metadata_count = input[offset]
-        offset += 1 
+        children_count = cursor.next
+        metadata_count = cursor.next
         children = [] of Node
         metadata = [] of Int32
 
@@ -63,18 +84,21 @@ class Node
         # afterwards. It's the only way to ensure that you're grabbing the 
         # correct elements as metadata.
         children_count.times do 
-            offset, child = Node.parse_helper(input, offset)
+            child = Node.parse_helper(cursor)
             children << child unless child.nil?
         end
+
         # Now grab the metadata, since the children are safely out of the way.
         metadata_count.times do 
-            metadata << input[offset]
-            offset += 1
+            metadata << cursor.next
         end
 
-        # Remember to include our current "scan offset" so we can report it to 
-        # any higher-up-the-stack "parent" calls in the recursion case.
-        return {offset, Node.new(children, metadata)}
+        # Et voila! Our tree is parsed! If that seems too simple, start from
+        # the base case (no children) and work your way up. 
+        # If 0 children, then no iteration through either of the .times loops. 
+        # If 1 child, grab headers, then grab child, then grab metadata, and done. 
+        # This holds true for N > 1 too. Recursion!
+        return Node.new(children, metadata)
     end
 end
 
