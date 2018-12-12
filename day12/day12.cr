80fd914f4d542deee32875785544e1e4a6d44291
spencerwi <spencerwi@gmail.com>
2018-12-12 14:08:09 -0500

    Attempting to use crazy bitwise ops for Day12

--- day12/day12.cr
+++ day12/day12.cr
@@ -1,80 +1,71 @@
-class Rule
-    getter pattern
-    getter? results_in_plant
-    def_equals @pattern, @results_in_plant
-
-    def initialize(@pattern : String, @results_in_plant : Bool)
-    end
-
-    def self.parse(str : String) : Rule
-        if /^(?<pots>[\.#]+) => (?<result>[\.#])$/ =~ str
-            pattern = $~["pots"]
-            results_in_plant = case $~["result"]
-                               when "." then false
-                               when "#" then true
-                               else raise "Invalid rule: #{str}"
-                               end
-            return Rule.new(pattern, results_in_plant)
-        else
-            raise "Invalid rule string: '#{str}'"
-        end
-    end
-
-    def matches?(pots : String) : Bool
-        return pots == @pattern
-    end
-end
-
-
 class Garden
-    getter pots, rules
+    getter pots, rules, leftmost_1
+    @position_of_original_0 : Int32
 
     def initialize(
-        @pots : Hash(Int32, Char),
-        @rules : Array(Rule), 
+        @pots : UInt32,
+        @rules : Hash(UInt32, Bool), 
     ) 
+        @position_of_original_0 = @pots.to_s(2).index('1').not_nil!
     end
 
     def self.parse(input_lines : Array(String)) : Garden
-        pots = Hash(Int32, Char).new('.')
         if /^initial state: (?<pots>[\.#]+)$/ =~ input_lines[0]
-            $~["pots"].chars.each_with_index do |c, idx|
-                pots[idx] = c
-            end
+            pots = $~["pots"].gsub("#", "1").gsub(".", "0").to_u32(base = 2)
         else 
             raise "Invalid initial state line: '#{input_lines[0]}'"
         end
 
-        rules = input_lines.skip(2).map {|line| Rule.parse(line)}
+        rules = self.parse_rules(input_lines.skip(2))
         return Garden.new(pots, rules)
     end
 
+    private def self.parse_rules(rule_strs : Array(String))
+        rules = Hash(UInt32, Bool).new(false)
+        rule_strs.each do |str|
+            if /^(?<pots>[\.#]{5}) => (?<result>[\.#])$/ =~ str
+                int_value = $~["pots"].gsub("#", "1").gsub(".", 0).to_u32(2)
+                results_in_plant = case $~["result"]
+                                   when "." then false
+                                   when "#" then true
+                                   else raise "Invalid rule: #{str}"
+                                   end
+                rules[int_value] = results_in_plant
+            else
+                raise "Invalid rule string: '#{str}'"
+            end
+        end
+        return rules
+    end
+
     def tick : Void
-        current_pot_state = @pots.clone
-        leftmost_filled_pot, rightmost_filled_pot = @pots.select{|idx, c| c == '#'}.keys.minmax
+        bitstring = @pots.to_s(base = 2)
 
         # We can only ever get a new plant in a pot that's 2 spaces away from
         # the farthest-out "plant-filled" pot.
-        range_to_examine = (leftmost_filled_pot - 2)..(rightmost_filled_pot + 2)
-        range_to_examine.each do |center_pot_index|
-            first_pot = center_pot_index - 2
-            last_pot = center_pot_index + 2
-            pot_group = (first_pot..last_pot).map {|idx| current_pot_state.fetch(idx, '.')}.join("")
+        leftmost_filled_pot = bitstring.index('1').not_nil!
+        rightmost_filled_pot = bitstring.rindex('1').not_nil!
+        new_pots_size = (rightmost_filled_pot + 2) - (leftmost_filled_pot - 2)
 
-            matching_rule = @rules.find(&.matches?(pot_group))
-            new_plant_state = if matching_rule && matching_rule.results_in_plant?
-                                  '#'
-                              else
-                                  '.'
-                              end
+        padded_bitstring = ("00" + bitstring[leftmost_filled_pot..rightmost_filled_pot] + "00").chars
+        new_bitstring = padded_bitstring.dup
 
-            @pots[center_pot_index] = new_plant_state
+        middle_index = 0
+        padded_bitstring.each_cons(size = 5) do |slice|
+            slice_as_uint = slice.join("").to_u32(2)
+            plant_in_middle = @rules[slice_as_uint]
+
+            new_bitstring[middle_index + 2] = plant_in_middle ? '1' : '0'
+            middle_index += 1
         end
+        @pots = new_bitstring.join("").to_u32(2)
+        @leftmost_1 = @pots
     end
 
     def to_s : String
-        smallest_seen_pot, largest_seen_pot = @pots.keys.minmax
-        return (smallest_seen_pot..largest_seen_pot).map {|idx| @pots[idx]}.join("")
+        @pots.to_s(2).gsub("1", "#").gsub("0", ".")
+        # smallest_seen_pot, largest_seen_pot = @pots.keys.minmax
+        # return (smallest_seen_pot..largest_seen_pot).map {|idx| @pots[idx]}.join("")
     end
 end
 
@@ -89,7 +80,14 @@ class Day12
         generation_count.times do
             @garden.tick
         end
-        return @garden.pots.select {|idx, c| c == '#'}.keys.sum
+        @garden.pots.to_s(2).reverse.chars.map_with_index do |char, idx|
+            if char == '1'
+                idx - @garden.offset
+            else
+                0
+            end
+        end.sum
+        #return @garden.pots.each_with_index.select(&.[0]).map(&.[1]).sum
     end
 
 end
--- day12/spec/day12_spec.cr
+++ day12/spec/day12_spec.cr
@@ -1,60 +1,54 @@
 require "../day12"
 require "spec"
 
-describe Rule do
+# describe Rule do
 
-    describe ".parse" do
+#     describe ".parse" do
 
-        it "errors on incorrect input" do
-            invalid_rules = ["", "Foo bar", "   ", "...#.. => ", ". => ?"]
-            invalid_rules.each do |invalid_rule|
-                expect_raises(Exception, "Invalid rule string: '#{invalid_rule}'") do 
-                    Rule.parse invalid_rule
-                end
-            end
-        end
+#         it "errors on incorrect input" do
+#             invalid_rules = ["", "Foo bar", "   ", "...#.. => ", ". => ?"]
+#             invalid_rules.each do |invalid_rule|
+#                 expect_raises(Exception, "Invalid rule string: '#{invalid_rule}'") do 
+#                     Rule.parse invalid_rule
+#                 end
+#             end
+#         end
 
-        it "parses correct inputs" do
-            valid_rules_with_expected_results = {
-                ". => ." => ".",
-                ". => #" => ".",
-                "# => #" => "#", 
-                "# => ." => "#", 
-                ".# => #" => ".#",
-                ".#. => #" => ".#."
-            }
+#         it "parses correct inputs" do
+#             valid_rules_with_expected_results = {
+#                 "..... => ." => {0, false},
+#                 "....# => #" => {1, true},
+#                 "...#. => #" => {2, true},
+#                 "..#.# => ." => {5, false},
+#                 ".#... => #" => {8, true},
+#                 "##### => #" => {1 + 2 + 4 + 8 + 16, true}
+#             }
 
-            valid_rules_with_expected_results.each do |rule_str, expected_result|
-                rule = Rule.parse(rule_str)
+#             valid_rules_with_expected_results.each do |rule_str, expected_result|
+#                 rule = Rule.parse(rule_str)
 
-                rule.pattern.should eq expected_result
-            end
-        end
-    end
+#                 rule.value.should eq expected_result[0]
+#                 rule.results_in_plant?.should eq expected_result[1]
+#             end
+#         end
+#     end
 
-    describe "#matches" do
+#     describe "#matches" do
 
-        it "should report matches correctly" do
-            rule = Rule.new(".#.##")
+#         it "should report matches correctly" do
+#             rule = Rule.new(8, true)
 
-            rule.matches?(".#.##").should be_true
-        end
+#             rule.matches?(8).should be_true
+#         end
 
-        it "should report mismatches correctly" do
-            rule = Rule.new(".#.##")
+#         it "should report mismatches correctly" do
+#             rule = Rule.new(8, true)
 
-            rule.matches?("##.##").should be_false
-            rule.matches?("...##").should be_false
-            rule.matches?(".####").should be_false
-            rule.matches?(".#..#").should be_false
-            rule.matches?(".#.#.").should be_false
-            rule.matches?("")
-            rule.matches?(".#.##.").should be_false
-            rule.matches?(".#.###").should be_false
-        end
+#             rule.matches?(1).should be_false
+#         end
 
-    end
-end
+#     end
+# end
 
 describe Garden do
 
@@ -85,22 +79,22 @@ describe Garden do
 
             garden.to_s.should eq("#..#.#..##......###...###")
 
-            garden.rules.should eq [
-                Rule.parse("...## => #"),
-                Rule.parse("..#.. => #"),
-                Rule.parse(".#... => #"),
-                Rule.parse(".#.#. => #"),
-                Rule.parse(".#.## => #"),
-                Rule.parse(".##.. => #"),
-                Rule.parse(".#### => #"),
-                Rule.parse("#.#.# => #"),
-                Rule.parse("#.### => #"),
-                Rule.parse("##.#. => #"),
-                Rule.parse("##.## => #"),
-                Rule.parse("###.. => #"),
-                Rule.parse("###.# => #"),
-                Rule.parse("####. => #"),
-            ]
+            garden.rules.should eq ({
+                "00011".to_u32(2) => true,
+                "00100".to_u32(2) => true,
+                "01000".to_u32(2) => true,
+                "01010".to_u32(2) => true,
+                "01011".to_u32(2) => true,
+                "01100".to_u32(2) => true,
+                "01111".to_u32(2) => true,
+                "10101".to_u32(2) => true,
+                "10111".to_u32(2) => true,
+                "11010".to_u32(2) => true,
+                "11011".to_u32(2) => true,
+                "11100".to_u32(2) => true,
+                "11101".to_u32(2) => true,
+                "11110".to_u32(2) => true,
+            })
         end
 
     end
@@ -144,9 +138,9 @@ describe Day12 do
     describe "#part_a" do 
 
         it "behaves correctly for sample input" do
-            day12 = Day12.new(sample_input.lines, 20)
+            day12 = Day12.new(sample_input.lines)
 
-            day12.part_a.should eq 325
+            day12.solve(20).should eq 325
         end
 
     end
