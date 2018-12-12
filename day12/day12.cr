0da69e1bba6064949d4642d2f42c560afe19a0e7
spencerwi <spencerwi@gmail.com>
2018-12-12 15:29:12 -0500

    Add full day12 solution

--- day12/day12.cr
+++ day12/day12.cr
@@ -1,79 +1,88 @@
-class Rule
-    getter pattern
-    getter? results_in_plant
-    def_equals @pattern, @results_in_plant
-
-    def initialize(@pattern : String, @results_in_plant : Bool)
-    end
-
-    def self.parse(str : String) : Rule
-        if /^(?<pots>[\.#]+) => (?<result>[\.#])$/ =~ str
-            pattern = $~["pots"]
-            results_in_plant = case $~["result"]
-                               when "." then false
-                               when "#" then true
-                               else raise "Invalid rule: #{str}"
-                               end
-            return Rule.new(pattern, results_in_plant)
-        else
-            raise "Invalid rule string: '#{str}'"
-        end
-    end
-
-    def matches?(pots : String) : Bool
-        return pots == @pattern
-    end
-end
-
-
 class Garden
     getter pots, rules
 
-    def initialize(
-        @pots : Hash(Int32, Char),
-        @rules : Array(Rule), 
-    ) 
+    # Rather than having an array where I have to keep growing it and
+    # keeping track of where the original "0" was, I instead use a Hash
+    # from index to char, which defaults to '.' for any index I haven't
+    # touched yet. That allows me to reach into negative or greater-than-max 
+    # indexes at will, providing automatic "growing" in both directions 
+    # without me having to do it manually.
+    def initialize(@pots : Hash(Int64, Char), @rules : Hash(String, Char)) 
     end
 
     def self.parse(input_lines : Array(String)) : Garden
-        pots = Hash(Int32, Char).new('.')
+        pots = Hash(Int64, Char).new('.')
         if /^initial state: (?<pots>[\.#]+)$/ =~ input_lines[0]
             $~["pots"].chars.each_with_index do |c, idx|
-                pots[idx] = c
+                pots[idx.to_i64] = c
             end
         else 
             raise "Invalid initial state line: '#{input_lines[0]}'"
         end
 
-        rules = input_lines.skip(2).map {|line| Rule.parse(line)}
+        rules = self.parse_rules(input_lines.skip(2))
         return Garden.new(pots, rules)
     end
 
+    # Rules, as it turns out, are just a mapping from each 5-pot "slice" of the
+    # whole garden to the resulting character that should be in the middle pot
+    # of the slice. If no rule matches, no plant should be in the middle pot,
+    # which maps really nicely to a hash with a default value of '.'
+    private def self.parse_rules(rule_lines : Array(String)) : Hash(String, Char)
+        rules = Hash(String, Char).new('.')
+        
+        rule_lines.each do |str|
+            if /^(?<pots>[\.#]+) => (?<result>[\.#])$/ =~ str
+                match_value = $~["pots"]
+                result = $~["result"].char_at(0)
+                rules[match_value] = result
+            else
+                raise "Invalid rule string: '#{str}'"
+            end
+        end
+
+        return rules
+    end
+
+    # Update the garden state by one step.
     def tick : Void
-        current_pot_state = @pots.clone
-        leftmost_filled_pot, rightmost_filled_pot = @pots.select{|idx, c| c == '#'}.keys.minmax
+        # Create a "working copy" so that we're not reading from and updating 
+        # the same data structure.
+        new_pot_state = @pots.dup
 
         # We can only ever get a new plant in a pot that's 2 spaces away from
-        # the farthest-out "plant-filled" pot.
+        # the farthest-out "plant-filled" pot. So we don't need to look further 
+        # in either direction than that.
+        leftmost_filled_pot, rightmost_filled_pot = @pots.select{|idx, c| c == '#'}.keys.minmax
         range_to_examine = (leftmost_filled_pot - 2)..(rightmost_filled_pot + 2)
+
         range_to_examine.each do |center_pot_index|
             first_pot = center_pot_index - 2
             last_pot = center_pot_index + 2
-            pot_group = (first_pot..last_pot).map {|idx| current_pot_state.fetch(idx, '.')}.join("")
+            pot_group = (first_pot..last_pot).map {|idx| @pots.fetch(idx, '.')}.join("")
 
-            matching_rule = @rules.find(&.matches?(pot_group))
-            new_plant_state = if matching_rule && matching_rule.results_in_plant?
-                                  '#'
-                              else
-                                  '.'
-                              end
-
-            @pots[center_pot_index] = new_plant_state
+            new_pot_state[center_pot_index] = @rules[pot_group]
         end
+
+        @pots = new_pot_state
     end
 
+    # "Scoring" a garden is actually pretty easy with the hash:
+    # find all the hash-keys that correspond to a '#', and you've got
+    # all your indexes (positive, negative, whatever). Then just sum them!
+    def score : Int64
+        @pots.select {|idx, c| c == '#'}
+            .keys
+            .map(&.to_i64).sum
+    end
+
+    # Convenience method used for TDD'ing my way to make sure that "tick" 
+    # behaves right by allowing me to compare my garden state to the sample.
     def to_s : String
         smallest_seen_pot, largest_seen_pot = @pots.keys.minmax
+        # We do the whole "range" trick to ensure a contiguous increase from 
+        # leftmost pot to rightmost pot, since we may have gaps due to our use
+        # of a hash rather than an array.
         return (smallest_seen_pot..largest_seen_pot).map {|idx| @pots[idx]}.join("")
     end
 end
@@ -85,11 +94,37 @@ class Day12
         @garden = Garden.parse(input_lines)
     end
 
-    def solve(generation_count : Int64)
-        generation_count.times do
+    def solve(generation_count : Int64, verbose = false)
+        previous_score = 0
+        last_score_change = 0
+        generation_count.times do |current_gen|
             @garden.tick
+
+            # As it turns out, eventually the "rate of score change" stabilizes, 
+            # probably due to repeating patterns like in Conway's Game of Life.
+            # So if we watch for the point where the score change "stabilizes",
+            # then we can just extrapolate the rest from there.
+            score_change = @garden.score - previous_score
+            puts "#{current_gen + 1}: #{@garden.score}, score change of #{score_change}" if verbose
+            if score_change == last_score_change
+                if verbose
+                    puts "The score is now constantly increasing by #{score_change}. We can extrapolate from here."
+                end
+
+                # If we've reached the point of constant increase, then we can 
+                # now extrapolate the results by just figuring out the number of
+                # generations left to simulate, and adding 
+                #   (rate * generations_left)
+                # to the our current score.
+                generations_left = generation_count - (current_gen + 1) # since current_gen is 0-indexed
+                amount_that_will_be_added_by_the_end = (generations_left * score_change)
+                return @garden.score + amount_that_will_be_added_by_the_end
+            end
+
+            previous_score = @garden.score
+            last_score_change = score_change
         end
-        return @garden.pots.select {|idx, c| c == '#'}.keys.sum
+        return @garden.score
     end
 
 end
@@ -97,6 +132,5 @@ end
 unless PROGRAM_NAME.includes?("crystal-run-spec")
     day12 = Day12.new(File.read_lines("input.txt"))
     puts "12A: #{day12.solve(20)}"
-    day12 = Day12.new(File.read_lines("input.txt"))
-    puts "12A: #{day12.solve(50_000_000_000)}"
+    puts "12B: #{day12.solve(50_000_000_000 - 20)}"
 end
--- day12/spec/day12_spec.cr
+++ day12/spec/day12_spec.cr
@@ -1,61 +1,6 @@
 require "../day12"
 require "spec"
 
-describe Rule do
-
-    describe ".parse" do
-
-        it "errors on incorrect input" do
-            invalid_rules = ["", "Foo bar", "   ", "...#.. => ", ". => ?"]
-            invalid_rules.each do |invalid_rule|
-                expect_raises(Exception, "Invalid rule string: '#{invalid_rule}'") do 
-                    Rule.parse invalid_rule
-                end
-            end
-        end
-
-        it "parses correct inputs" do
-            valid_rules_with_expected_results = {
-                ". => ." => ".",
-                ". => #" => ".",
-                "# => #" => "#", 
-                "# => ." => "#", 
-                ".# => #" => ".#",
-                ".#. => #" => ".#."
-            }
-
-            valid_rules_with_expected_results.each do |rule_str, expected_result|
-                rule = Rule.parse(rule_str)
-
-                rule.pattern.should eq expected_result
-            end
-        end
-    end
-
-    describe "#matches" do
-
-        it "should report matches correctly" do
-            rule = Rule.new(".#.##")
-
-            rule.matches?(".#.##").should be_true
-        end
-
-        it "should report mismatches correctly" do
-            rule = Rule.new(".#.##")
-
-            rule.matches?("##.##").should be_false
-            rule.matches?("...##").should be_false
-            rule.matches?(".####").should be_false
-            rule.matches?(".#..#").should be_false
-            rule.matches?(".#.#.").should be_false
-            rule.matches?("")
-            rule.matches?(".#.##.").should be_false
-            rule.matches?(".#.###").should be_false
-        end
-
-    end
-end
-
 describe Garden do
 
     sample_input = <<-INPUT
@@ -85,22 +30,22 @@ describe Garden do
 
             garden.to_s.should eq("#..#.#..##......###...###")
 
-            garden.rules.should eq [
-                Rule.parse("...## => #"),
-                Rule.parse("..#.. => #"),
-                Rule.parse(".#... => #"),
-                Rule.parse(".#.#. => #"),
-                Rule.parse(".#.## => #"),
-                Rule.parse(".##.. => #"),
-                Rule.parse(".#### => #"),
-                Rule.parse("#.#.# => #"),
-                Rule.parse("#.### => #"),
-                Rule.parse("##.#. => #"),
-                Rule.parse("##.## => #"),
-                Rule.parse("###.. => #"),
-                Rule.parse("###.# => #"),
-                Rule.parse("####. => #"),
-            ]
+            garden.rules.should eq ({
+                "...##" => '#',
+                "..#.." => '#',
+                ".#..." => '#',
+                ".#.#." => '#',
+                ".#.##" => '#',
+                ".##.." => '#',
+                ".####" => '#',
+                "#.#.#" => '#',
+                "#.###" => '#',
+                "##.#." => '#',
+                "##.##" => '#',
+                "###.." => '#',
+                "###.#" => '#',
+                "####." => '#',
+            })
         end
 
     end
@@ -113,6 +58,7 @@ describe Garden do
             garden.tick
             puts garden.to_s
 
+            # Their 1-generation sample output is weirdly longer than mine
             "...#...#....#.....#..#..#..#...........".should contain(garden.to_s)
         end
 
@@ -144,9 +90,9 @@ describe Day12 do
     describe "#part_a" do 
 
         it "behaves correctly for sample input" do
-            day12 = Day12.new(sample_input.lines, 20)
+            day12 = Day12.new(sample_input.lines)
 
-            day12.part_a.should eq 325
+            day12.solve(20).should eq 325
         end
 
     end
