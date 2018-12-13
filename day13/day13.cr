2681c19033493b8b26a4f0550da768b15498a60b
spencerwi <spencerwi@gmail.com>
2018-12-13 16:06:42 -0500

    Clean up and comment Day13 solution

--- day13/day13.cr
+++ day13/day13.cr
@@ -4,7 +4,7 @@ module Directions
         Straight,
         Right
 
-        def next
+        def next : Turn
             Turn.from_value((self.value + 1) % 3)
         end
     end
@@ -15,7 +15,7 @@ module Directions
         West,
         South
 
-        def self.of_char(c : Char)
+        def self.of_char(c : Char) : Compass
             case c 
             when '^' then North
             when '>' then East
@@ -25,17 +25,23 @@ module Directions
             end
         end
 
-        def to_char
+        def to_char : Char
+            # .to_char is just the reverse of .of_char.
             ['^', '>', 'v', '<'].find {|c| Compass.of_char(c) == self}.not_nil!
         end
 
-        def replacement_track_char
+        # We want a "clean" grid to work from so that carts trying to figure out
+        # what the track looks like under them don't see another cart instead of
+        # the track.
+        def replacement_track_char : Char
             case self
             when North, South then '|'
             when East, West then '-'
             end.not_nil!
         end
 
+        # If I'm facing a given compass direction, and I want to turn in some
+        # turn-direction, what direction will I be facing then?
         def turn(turn : Turn) : Compass
             return self if turn.straight?
 
@@ -66,10 +72,7 @@ module Directions
 end
 
 class Cart
-    include Directions
-    def self.is_cart_char?(c : Char)
-        ['>', '^', 'v', '<'].includes?(c)
-    end
+    include Directions # So I don't have to keep typing Directions::
 
     getter direction, x, y
     @direction : Compass
@@ -80,15 +83,21 @@ class Cart
         @next_turn = Turn::Left
     end
 
+    def self.is_cart_char?(c : Char)
+        ['>', '^', 'v', '<'].includes?(c)
+    end
+
     def to_char 
         @direction.to_char
     end
 
+    # Updates the cart by moving one step in the appropriate direction and 
+    # turning if needed.
     def tick(grid : Array(Array(Char)))
-        char_under_cart = grid[@y][@x]
-        case char_under_cart
+        track_under_cart = grid[@y][@x]
+        case track_under_cart
         when '|', '-' then self.move(@direction)
-        when '\\', '/' then self.handle_curve(char_under_cart)
+        when '\\', '/' then self.handle_curve(track_under_cart)
         when '+' then self.handle_intersection
         end
     end
@@ -103,6 +112,7 @@ class Cart
     end
 
     private def handle_curve(curve_char : Char)
+        # Turn towards the appropriate direction first
         @direction =
             if curve_char == '\\'
                 case @direction 
@@ -119,12 +129,18 @@ class Cart
                 when .south? then Compass::West
                 end
             end.not_nil!
+
+        # Then move in that direction
         self.move(@direction)
     end
 
     private def handle_intersection
+        # First, turn towards the appropriate compass direction, based on what 
+        # our next turn-direction should be,
         @direction = @direction.turn(@next_turn)
+        # then update our memory of what our next turn-direction should be,
         @next_turn = @next_turn.next
+        # and then move.
         self.move(@direction)
     end
 
@@ -132,26 +148,29 @@ end
 
 class Day13
 
+    # Used by `#tick` to determine how to handle crashes
     enum CrashBehavior
         ReportFirst
         RemoveCrashedCarts
     end
 
     getter grid, carts
-
-    @carts : Array(Cart)
     @grid : Array(Array(Char))
+    @carts : Array(Cart)
 
     def initialize(input_lines : Array(String))
-        @grid = input_lines.map(&.chars)
-
-        @carts = Array(Cart).new
-        @grid.each_with_index do |row, y|
-            row.each_with_index do |char, x|
+        @carts = [] of Cart
+        @grid = input_lines.map_with_index do |row, y|
+            row.chars.map_with_index do |char, x|
+                # Parse and replace each cart character with the appropriate 
+                # kind of track, so that our grid is "clean" and we know for
+                # sure what kind of track is at each spot.
                 if Cart.is_cart_char?(char)
                     cart = Cart.new(char, x, y)
-                    @grid[y][x] = cart.direction.replacement_track_char
                     @carts << cart
+                    cart.direction.replacement_track_char
+                else
+                    char
                 end
             end
         end
@@ -181,24 +200,31 @@ class Day13
     end
 
 
-    # Advances carts one at a time, checking for collisions and reporting the
-    # location of the first collision, if any occurred.
+    # Advances carts one at a time, checking for collisions and handling the 
+    # collision based on the provided crash_behavior. If crash_behavior is
+    # `ReportFirst`, then we return immediately on identifying a collision to 
+    # report its location. If crash_behavior is `RemoveCrashedCarts`, then we 
+    # continue in the case of crashes, but we remove the crashed carts from our 
+    # list of carts (and eventually just return nil, since there's nothing 
+    # interesting to report).
     def tick(crash_behavior : CrashBehavior) : Tuple(Int32, Int32)?
         new_carts = @carts.dup
 
-        # Move all carts, starting from the top-left one
+        # Move all carts one at a time, starting from the top-left one
         @carts.sort_by {|c| {c.y, c.x} }.each do |cart| 
             cart.tick(@grid)
+            # Look for carts already in the spot we just moved into.
             carts_already_occupying_new_position = 
                 @carts.reject {|other| other == cart}
                 .select {|other| other.x == cart.x && other.y == cart.y}
 
-            if carts_already_occupying_new_position.size > 0
+            # If there's a crash, then we go into crash_behavior
+            unless carts_already_occupying_new_position.empty?
                 if crash_behavior.report_first?
                     return {cart.x, cart.y}
                 elsif crash_behavior.remove_crashed_carts?
-                    # remove all crashed carts
-                    new_carts.delete(cart) # this one
+                    # Remove all crashed carts.
+                    new_carts.delete(cart) # First the current one
                     carts_already_occupying_new_position.each {|c| new_carts.delete(c)} # and any others on that spot
                 end
             end
@@ -208,11 +234,13 @@ class Day13
         return nil
     end
 
+    # A convenience method allowing us to animate the simulation in a terminal
     private def redraw_sim_output
         puts `clear`
         puts self.to_s
     end
 
+    # A convenience method for drawing the track, used particularly in specs.
     def to_s
         @grid.map_with_index do |row, y|
             row.map_with_index do |char, x|
