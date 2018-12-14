033f57c8b58acf8f942dd849aa1e6112f0058b39
spencerwi <spencerwi@gmail.com>
2018-12-14 13:28:17 -0500

    Day14 part b solved

--- day14/day14.cr
+++ day14/day14.cr
@@ -20,30 +20,45 @@ class Day14
     end
 
     # Part B problem statement: how many recipes precede the input int as a 
-    # sequence of scores?
+    # sequence of digits?
     def part_b : Int32
+
         # Since we only ever tack new stuff onto the end of the scoreboard, we 
         # only need to search through the whole scoreboard once. After that, we
         # can just slice a hunk off the end big enough that it could possibly be
         # the digit sequence we need, and check that.
         input_digits = @input.chars.map(&.to_i)
         index_of_chars = @scoreboard.each_cons(@input.size).index(input_digits)
-        while index_of_chars.nil?
-            self.tick
+        return index_of_chars unless index_of_chars.nil?
 
-            unless @scoreboard.size < @input.size
-                slice_start = @scoreboard.size - @input.size
-                slice_off_end = (slice_start...@scoreboard.size).map {|idx| @scoreboard[idx]}.to_a
+        loop do
+            # If it wasn't already found, we keep on ticking until one of the 
+            # "tails" of the scoreboard matches the input
+            tails = self.tick
 
-                if slice_off_end == input_digits
-                    index_of_chars = slice_start
+            # This is a little hairy, but it is so because `self.tick` returns
+            # either one tail if we only added one recipe, or two tails if we 
+            # added two. 
+            case tails.size
+            when 2 then # if we got back two tails, we added two recipes.
+                if tails[0] == input_digits # if the first tail matches, 
+                    return @scoreboard.size - @input.size - 1 # the second-to-last 5 characters match.
+                elsif tails[1] == input_digits # if the second tail matches,
+                    return @scoreboard.size - @input.size # the last 5 characters match.
+                end
+            when 1 then # if we only got back 1 tail, we added one recipe.
+                if tails[0] == input_digits
+                    return @scoreboard.size - @input.size # the last 5 characters match
                 end
             end
         end
-        return index_of_chars.not_nil!
     end
 
-    def tick
+    # Does the recipes, updates the scoreboard, and moves the elves. 
+    # When finished, it returns each "tail-end" of the scoreboard that we've 
+    # seen. If we added just one recipe to the scoreboard, you get a single
+    # `@input.size`-digit array. If we added two, you get both.
+    def tick : Array(Array(Int32))
         # Get the elves' combined recipe score
         elf1, elf2 = @elf_indexes
         recipe1, recipe2 = {
@@ -53,19 +68,29 @@ class Day14
         elf_score = recipe1 + recipe2
 
         # Tack the digits of the elves' score onto the end of the scoreboard.
+        scoreboard_tails = [] of Array(Int32)
         if elf_score >= 10
             first_digit, second_digit = elf_score.divmod(10)
             @scoreboard << first_digit
+            scoreboard_tails << self.get_scoreboard_tail
             @scoreboard << second_digit
         else 
             @scoreboard << elf_score
         end
+        scoreboard_tails << self.get_scoreboard_tail
 
         # Move the elves
         @elf_indexes = {
             (elf1 + recipe1 + 1) % @scoreboard.size,
             (elf2 + recipe2 + 1) % @scoreboard.size
         }
+        return scoreboard_tails
+    end
+
+    # Gets the last `@input.size` digits of the scoreboard.
+    private def get_scoreboard_tail : Array(Int32)
+        slice_start = (@scoreboard.size - @input.size)
+        return (slice_start...@scoreboard.size).map {|idx| @scoreboard[idx]}.to_a
     end
 
     def to_s : String
