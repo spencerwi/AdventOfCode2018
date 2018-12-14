d459482ec43a3b1bd43ace83cb1f258bd11d55c4
spencerwi <spencerwi@gmail.com>
2018-12-14 10:56:54 -0500

    Tweak day14 perf a bit, still really slow

--- day14/day14.cr
+++ day14/day14.cr
@@ -16,24 +16,41 @@ class Day14
             self.tick
             # puts self.to_s
         end
-        return (input_int..input_int+9).map {|idx| @scoreboard[idx]}.map(&.to_s).join("")
+        return (input_int..input_int + 9).map {|idx| @scoreboard[idx]}.map(&.to_s).join("")
     end
 
     # Part B problem statement: how many recipes precede the input int as a 
     # sequence of scores?
     def part_b : Int32
-        stringified = @scoreboard.map(&.to_s).join("")
-        until stringified.includes?(@input)
+        # Since we only ever tack new stuff onto the end of the scoreboard, we 
+        # only need to search through the whole scoreboard once. After that, we
+        # can just slice a hunk off the end big enough that it could possibly be
+        # the digit sequence we need, and check that.
+        input_digits = @input.chars.map(&.to_i)
+        index_of_chars = @scoreboard.each_cons(@input.size).index(input_digits)
+        while index_of_chars.nil?
             self.tick
-            stringified = @scoreboard.map(&.to_s).join("")
+
+            unless @scoreboard.size < @input.size
+                slice_start = @scoreboard.size - @input.size
+                slice_off_end = (slice_start...@scoreboard.size).map {|idx| @scoreboard[idx]}.to_a
+
+                if slice_off_end == input_digits
+                    index_of_chars = slice_start
+                end
+            end
         end
-        return stringified.index(@input).not_nil!
+        return index_of_chars.not_nil!
     end
 
     def tick
         # Get the elves' combined recipe score
-        elf_recipes = @elf_indexes.map {|idx| @scoreboard[idx]}
-        elf_score = elf_recipes.sum
+        elf1, elf2 = @elf_indexes
+        recipe1, recipe2 = {
+            @scoreboard[elf1],
+            @scoreboard[elf2]
+        }
+        elf_score = recipe1 + recipe2
 
         # Tack the digits of the elves' score onto the end of the scoreboard.
         if elf_score >= 10
@@ -44,11 +61,11 @@ class Day14
             @scoreboard << elf_score
         end
 
-        @elf_indexes = elf_recipes.map_with_index do |recipe_value, elf_number| 
-            # Each elf steps forward from their current position by their
-            # recipe value plus 1, wrapping around as needed.
-            (@elf_indexes[elf_number] + recipe_value + 1) % @scoreboard.size 
-        end
+        # Move the elves
+        @elf_indexes = {
+            (elf1 + recipe1 + 1) % @scoreboard.size,
+            (elf2 + recipe2 + 1) % @scoreboard.size
+        }
     end
 
     def to_s : String
@@ -61,7 +78,8 @@ class Day14
 end
 
 unless PROGRAM_NAME.includes?("crystal-run-spec")
-    day14 = Day14.new("890691")
+    # day14 = Day14.new("890691")
+    day14 = Day14.new("2018")
     puts "14A: #{day14.part_a}"
     puts "14B: #{day14.part_b}"
 end
