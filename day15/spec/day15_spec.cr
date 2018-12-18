ddc8a63e418bd5694a3d19a07d02f231d0be0ed7
spencerwi <spencerwi@gmail.com>
2018-12-18 09:06:25 -0500

    More work on Day15. It actually works for some test cases now.

--- day15/spec/day15_spec.cr
+++ day15/spec/day15_spec.cr
@@ -1,37 +1,32 @@
 require "./spec_helper"
 
-describe Elf do
+describe Warrior do
 
   it "starts with 200 hp" do
-    Elf.new.hp.should eq 200
+    Warrior.new(Warrior::Team::Elf).hp.should eq 200
   end
 
-  it "hates Goblins" do
-    Elf.new.hates?(Goblin.new).should be_true
-  end
-
-  it "doesn't hate anything else" do
-    elf = Elf.new
-    [Elf, Wall, EmptySpace].each do |type|
-      element_of_type = type.new
-      elf.hates?(element_of_type).should be_false
-    end
+  it "hates the other team" do
+		elf = Warrior.new(Warrior::Team::Elf)
+		goblin = Warrior.new(Warrior::Team::Goblin)
+		elf.hates?(goblin).should be_true
+		goblin.hates?(elf).should be_true
   end
 
   describe "#is_alive?" do
 
     it "reports true if health is greater than 0" do
-      Elf.new.is_alive?.should be_true
+      Warrior.new(Warrior::Team::Elf).is_alive?.should be_true
     end
 
     it "reports false if health is 0" do
-      elf = Elf.new
+      elf = Warrior.new(Warrior::Team::Elf)
       elf.hp = 0
       elf.is_alive?.should be_false
     end
 
     it "reports false if health is less than 0" do
-      elf = Elf.new
+      elf = Warrior.new(Warrior::Team::Elf)
       elf.hp = -1
       elf.is_alive?.should be_false
     end
@@ -41,8 +36,8 @@ describe Elf do
   describe "#attack" do
 
     it "avoids friendly fire" do
-      elf1 = Elf.new
-      elf2 = Elf.new
+      elf1 = Warrior.new(Warrior::Team::Elf)
+      elf2 = Warrior.new(Warrior::Team::Elf)
 
       elf1.attack(elf2)
 
@@ -50,120 +45,60 @@ describe Elf do
     end
 
     it "hurts enemies" do
-      elf = Elf.new
-      goblin = Goblin.new
+      elf = Warrior.new(Warrior::Team::Elf)
+      goblin = Warrior.new(Warrior::Team::Goblin)
 
       elf.attack(goblin)
 
       goblin.hp.should eq 197
+
+			goblin.attack(elf)
+
+			elf.hp.should eq 197
     end
 
     it "kills a nearly-dead enemy" do
-      elf = Elf.new
-      nearly_dead_goblin = Goblin.new
+      elf = Warrior.new(Warrior::Team::Elf)
+      nearly_dead_goblin = Warrior.new(Warrior::Team::Goblin)
       nearly_dead_goblin.hp = 3
 
       elf.attack(nearly_dead_goblin)
 
       nearly_dead_goblin.is_alive?.should be_false
+
+
+			goblin = Warrior.new(Warrior::Team::Goblin)
+			nearly_dead_elf = Warrior.new(Warrior::Team::Elf)
+			nearly_dead_elf.hp = 3
+
+      goblin.attack(nearly_dead_elf)
+
+      nearly_dead_elf.is_alive?.should be_false
     end
 
     it "doesn't reduce hp below 0" do
-      elf = Elf.new
-      nearly_dead_goblin = Goblin.new
+      elf = Warrior.new(Warrior::Team::Elf)
+      nearly_dead_goblin = Warrior.new(Warrior::Team::Goblin)
       nearly_dead_goblin.hp = 1
 
       elf.attack(nearly_dead_goblin)
 
       nearly_dead_goblin.is_alive?.should be_false
       nearly_dead_goblin.hp.should eq 0
+
+
+			goblin = Warrior.new(Warrior::Team::Goblin)
+			nearly_dead_elf = Warrior.new(Warrior::Team::Elf)
+			nearly_dead_elf.hp = 1
+
+      goblin.attack(nearly_dead_elf)
+
+      nearly_dead_elf.is_alive?.should be_false
+      nearly_dead_elf.hp.should eq 0
     end
 
   end
 
-end
-
-describe Goblin do
-
-  it "starts with 200 hp" do
-    Goblin.new.hp.should eq 200
-  end
-
-  it "hates Elves" do
-    Goblin.new.hates?(Elf.new).should be_true
-  end
-
-  it "doesn't hate anything else" do
-    goblin = Goblin.new
-    [Goblin, Wall, EmptySpace].each do |type|
-      element_of_type = type.new
-      goblin.hates?(element_of_type).should be_false
-    end
-  end
-
-  describe "#is_alive?" do
-
-    it "reports true if health is greater than 0" do
-      Goblin.new.is_alive?.should be_true
-    end
-
-    it "reports false if health is 0" do
-      goblin = Goblin.new
-      goblin.hp = 0
-      goblin.is_alive?.should be_false
-    end
-
-    it "reports false if health is less than 0" do
-      goblin = Goblin.new
-      goblin.hp = -1
-      goblin.is_alive?.should be_false
-    end
-
-  end
-
-  describe "#attack" do
-
-    it "avoids friendly fire" do
-      goblin1 = Goblin.new
-      goblin2 = Goblin.new
-
-      goblin1.attack(goblin2)
-
-      goblin2.hp.should eq 200
-    end
-
-    it "hurts enemies" do
-      goblin = Goblin.new
-      elf = Elf.new
-
-      goblin.attack(elf)
-
-      elf.hp.should eq 197
-
-      it "kills a nearly-dead enemy" do
-        goblin = Goblin.new
-        nearly_dead_elf = Elf.new
-        nearly_dead_elf.hp = 3
-
-        goblin.attack(nearly_dead_elf)
-
-        nearly_dead_elf.is_alive?.should be_false
-      end
-
-      it "doesn't reduce hp below 0" do
-        goblin = Goblin.new
-        nearly_dead_elf = Elf.new
-        nearly_dead_elf.hp = 1
-
-        goblin.attack(nearly_dead_elf)
-
-        nearly_dead_elf.is_alive?.should be_false
-        nearly_dead_elf.hp.should eq 0
-      end
-
-    end
-
-  end
 end
 
 describe Grid do
@@ -198,17 +133,260 @@ describe Grid do
       end
 
       # Check goblin positions
-      grid[1,1].should be_a(Goblin)
-      grid[1,4].should be_a(Goblin)
-      grid[1,7].should be_a(Goblin)
-      grid[4,1].should be_a(Goblin)
-      grid[4,7].should be_a(Goblin)
-      grid[7,1].should be_a(Goblin)
-      grid[7,4].should be_a(Goblin)
-      grid[7,7].should be_a(Goblin)
+      grid[1,1].as(Warrior).team.should eq Warrior::Team::Goblin
+      grid[1,4].as(Warrior).team.should eq Warrior::Team::Goblin
+      grid[1,7].as(Warrior).team.should eq Warrior::Team::Goblin
+      grid[4,1].as(Warrior).team.should eq Warrior::Team::Goblin
+      grid[4,7].as(Warrior).team.should eq Warrior::Team::Goblin
+      grid[7,1].as(Warrior).team.should eq Warrior::Team::Goblin
+      grid[7,4].as(Warrior).team.should eq Warrior::Team::Goblin
+      grid[7,7].as(Warrior).team.should eq Warrior::Team::Goblin
 
       # Check elf position
-      grid[4,4].should be_a(Elf)
+      grid[4,4].as(Warrior).team.should eq Warrior::Team::Elf
+    end
+  end
+
+	describe "#winning_side" do
+
+		it "correctly identifies a no-winner-yet situation" do
+      sample_input = <<-INPUT
+      #######
+      #.G...#
+      #...EG#
+      #.#.#G#
+      #..G#E#
+      #.....#
+      #######
+      INPUT
+      grid = Grid.parse(sample_input.lines)
+
+			grid.winning_side.should be_nil
+		end
+
+		it "correctly identifies a Goblins-win situation" do
+      sample_input = <<-INPUT
+      #######
+      #.G...#
+      #....G#
+      #.#.#G#
+      #..G#.#
+      #.....#
+      #######
+      INPUT
+      grid = Grid.parse(sample_input.lines)
+
+			grid.winning_side.should eq Warrior::Team::Goblin
+		end
+
+		it "correctly identifies an Elves-win situation" do
+      sample_input = <<-INPUT
+      #######
+      #.....#
+      #...E.#
+      #.#.#.#
+      #...#E#
+      #.....#
+      #######
+      INPUT
+      grid = Grid.parse(sample_input.lines)
+
+			grid.winning_side.should eq Warrior::Team::Elf
+		end
+
+	end
+
+  describe "#tick" do
+
+    it "behaves correctly for sample input" do
+      round_count = 0
+      sample_input = <<-INPUT
+      #######
+      #.G...#
+      #...EG#
+      #.#.#G#
+      #..G#E#
+      #.....#
+      #######
+      INPUT
+
+      grid = Grid.parse(sample_input.lines)
+
+      grid.tick
+      round_count += 1
+      grid.to_s(true).should eq <<-OUTPUT
+      #######
+      #..G..# G(200)
+      #...EG# E(197), G(197)
+      #.#G#G# G(200), G(197)
+      #...#E# E(197)
+      #.....#
+      #######
+      OUTPUT
+
+      grid.tick
+      round_count += 1
+      grid.to_s(true).should eq <<-OUTPUT
+      #######
+      #...G.# G(200)
+      #..GEG# G(200), E(188), G(194)
+      #.#.#G# G(194)
+      #...#E# E(194)
+      #.....#
+      #######
+      OUTPUT
+
+      21.times do
+        grid.tick
+        round_count += 1
+      end
+      grid.to_s.should eq <<-OUTPUT
+      #######
+      #...G.#
+      #..G.G#
+      #.#.#G#
+      #...#E#
+      #.....#
+      #######
+      OUTPUT
+
+      grid.tick
+      round_count += 1
+			grid.to_s.should eq <<-OUTPUT
+			#######
+			#..G..#
+			#...G.#
+			#.#G#G#
+			#...#E#
+			#.....#
+			#######
+			OUTPUT
+
+      grid.tick
+      round_count += 1
+			grid.to_s.should eq <<-OUTPUT
+			#######
+			#.G...#
+			#..G..#
+			#.#.#G#
+			#..G#E#
+			#.....#
+			#######
+			OUTPUT
+
+      grid.tick
+      round_count += 1
+			grid.to_s.should eq <<-OUTPUT
+			#######
+			#G....#
+			#.G...#
+			#.#.#G#
+			#...#E#
+			#..G..#
+			#######
+			OUTPUT
+
+      grid.tick
+      round_count += 1
+			grid.to_s.should eq <<-OUTPUT
+			#######
+			#G....#
+			#.G...#
+			#.#.#G#
+			#...#E#
+			#...G.#
+			#######
+			OUTPUT
+
+      grid.tick
+      round_count += 1
+			grid.to_s.should eq <<-OUTPUT
+			#######
+			#G....#
+			#.G...#
+			#.#.#G#
+			#...#E#
+			#....G#
+			#######
+			OUTPUT
+
+			(41 - 22).times do
+        grid.tick
+        round_count += 1
+      end
+			grid.to_s(true).should eq <<-OUTPUT
+			#######
+			#G....# G(200)
+			#.G...# G(131)
+			#.#.#G# G(59)
+			#...#.#
+			#....G# G(200)
+			#######
+			OUTPUT
+
+      puts "round_count #{round_count}"
+      puts "score: #{grid.score}"
+
+      # When a warrior notices that there are no more targets, then it should
+      # raise the alarm that the battle is over, and that should bubble up
+      # through grid.tick
+      expect_raises(Warrior::BattleIsOver) { grid.tick }
+
+    end
+
+    it "works for problematic inputs" do
+
+      input = <<-INPUT
+      #######
+      #######
+      #.E..G#
+      #.#####
+      #G#####
+      #######
+      #######
+      INPUT
+
+      grid = Grid.parse(input.lines)
+      grid.tick
+      grid.to_s.should eq <<-OUTPUT
+      #######
+      #######
+      #..EG.#
+      #G#####
+      #.#####
+      #######
+      #######
+      OUTPUT
+
+      input = <<-INPUT
+      ####
+      #.G#
+      #GE#
+      ####
+      INPUT
+
+      grid = Grid.parse(input.lines)
+      grid.tick
+      grid.to_s.should eq input
+      grid[2,1].as(Warrior).hp.should eq 197 # elf should attack top goblin
+      grid[2,2].as(Warrior).hp.should eq 194 # elf should get attacked twice
+
+
+      input = <<-INPUT
+      ########
+      #..E..G#
+      #G######
+      ########
+      INPUT
+
+      grid = Grid.parse(input.lines)
+      grid.tick
+      grid.to_s.should eq <<-OUTPUT
+      ########
+      #GE..G.#
+      #.######
+      ########
+      OUTPUT
     end
   end
 end
@@ -229,7 +407,9 @@ describe Day15 do
       INPUT
 
       day15 = Day15.new(sample_input.lines)
-      day15.part_a.should eq 36334
+      result = day15.part_a(true)
+			puts day15.grid.to_s(true)
+			result.should eq 36334
     end
   end
 
--- day15/src/day15.cr
+++ day15/src/day15.cr
@@ -1,38 +1,77 @@
 require "pathfinding" # Pull in a BFS lib to do pathfinding on the grid
 
+alias X = Int32
+alias Y = Int32
+alias Coords = Tuple(X, Y)
+
 abstract class GridElement
-  abstract def tick(grid : Grid, own_coords : Coords)
   abstract def to_char : Char
 end
 
-abstract class Warrior < GridElement
+class EmptySpace < GridElement
+  def to_char
+    '.'
+  end
+end
+
+class Wall < GridElement
+  def to_char
+    '#'
+  end
+end
+
+class Warrior < GridElement
+  class BattleIsOver < Exception
+  end
+
+	enum Team
+		Elf,
+    Goblin
+	end
+
   @hp : Int32
 
   property hp
-  def initialize
+	getter team
+  def initialize(@team : Team)
     @hp = 200
   end
 
-  def tick(grid : Grid, own_coords : Coords)
-    enemies_in_attack_range : Array(Warrior) = grid.neighbors(own_coords)
-      .map {|coords, other_element| other_element}
-      .select {|other| self.hates?(other)}
-      .map(&.as(Warrior))
+  # Returns Coords for where the Warrior should be at the end of its turn.
+  # Raises `BattleIsOver` if there are no more enemies, since the stopping
+  # condition for the simulation is that a *warrior notices* that there are
+  # no more enemies.
+  def tick(grid : Grid, own_coords : Coords, verbose : Bool = false) : Coords
+    enemies_in_range = self.find_enemies_in_attack_range(grid, own_coords)
+    enemy_to_attack = enemies_in_range.first?
+    puts "I'm #{self.to_s} @ #{own_coords}" if verbose
+    puts "Enemies in range: #{enemies_in_range.map(&.to_s)}, Enemy to attack: #{enemy_to_attack.to_s}" if verbose
 
-    if enemies_in_attack_range.empty?
-      self.move(grid, own_coords)
-    else
-      # TODO: pick an enemy and attack
-      self.attack(enemies_in_attack_range.first.not_nil!)
+		# If there's nobody right next to us to attack, then move closer
+		final_destination =
+			if enemy_to_attack.nil?
+				self.move(grid, own_coords)
+			else
+				own_coords
+			end
+
+		# If there's anyone to attack now, then do it.
+    if enemy_to_attack.nil?
+      enemies_in_range = self.find_enemies_in_attack_range(grid, final_destination)
+      enemy_to_attack = enemies_in_range.first?
+      puts "Enemies in range: #{enemies_in_range.map(&.to_s)}, Enemy to attack: #{enemy_to_attack.to_s}" if verbose
     end
+		self.attack(enemy_to_attack) unless enemy_to_attack.nil?
+		return final_destination
   end
 
-  def move(grid : Grid, own_coords : Coords)
+  # Returns Coords for where the warrior should be at the end of its turn
+  def move(grid : Grid, own_coords : Coords) : Coords
     targets = self.find_targets(grid)
-    return if targets.empty?
+    raise BattleIsOver.new if targets.empty?
 
     spaces_in_range_of_targets = targets.flat_map do |target_coords, target|
-      grid.neighbors(target_coords).select(&.is_a?(EmptySpace))
+      grid.neighbors(target_coords).select(&.[1].is_a?(EmptySpace))
     end
 
     paths_to_attack_targets = spaces_in_range_of_targets.map do |space_coords, space|
@@ -40,26 +79,20 @@ abstract class Warrior < GridElement
     end.reject {|_, path| path.nil?}
       .to_h
 
-    return if paths_to_attack_targets.empty?
+    return own_coords if paths_to_attack_targets.empty?
 
-    destination, _ = paths_to_attack_targets.min_by do |space_coords, path|
+    target_square, path_to_target_square = paths_to_attack_targets.min_by do |space_coords, path|
+      # Choose the shortest path. If there's a tie, choose the top-left-most
+      # space as your next step.
       x, y = space_coords
-      {path.not_nil!.size, x, y}
+      {path.not_nil!.size, y, x}
     end
 
-    if destination && destination[1]
-      current_x, current_y = own_coords
-      dest_x, dest_y = destination.not_nil!
-      grid[current_x, current_y] = EmptySpace.new
-      grid[dest_x, dest_y] = self
+    if path_to_target_square && path_to_target_square[1]
+      return path_to_target_square[1]
+    else
+      return own_coords
     end
-  end
-
-  abstract def hates?(other : GridElement)
-
-  def find_targets(grid : Grid) : Array(Tuple(Coords, Warrior))
-    grid.cells.select {|coords, cell| self.hates?(cell) }
-      .map {|coords, cell| {coords, cell.as(Warrior)} }
   end
 
   def attack(other : Warrior)
@@ -68,57 +101,42 @@ abstract class Warrior < GridElement
     end
   end
 
+  def hates?(other : GridElement)
+		return other.is_a?(Warrior) && other.team != @team
+	end
+
   def is_alive?
     @hp > 0
   end
+
+	def to_char
+		case @team
+			when .elf? then 'E'
+			when .goblin? then 'G'
+			else '.'
+		end
+	end
+
+	def to_s
+		"#{self.to_char}(#{@hp})"
+	end
+
+  private def find_targets(grid : Grid) : Array(Tuple(Coords, Warrior))
+    grid.cells.select {|coords, cell| self.hates?(cell) }
+      .map {|coords, cell| {coords, cell.as(Warrior)} }
+  end
+
+	private def find_enemies_in_attack_range(grid : Grid, own_coords : Coords) : Array(Warrior)
+		grid.neighbors(own_coords)
+			.select {|coords, other| self.hates?(other)}
+      .map {|coords, other_element| {coords, other_element.as(Warrior)}}
+      .sort_by do |coords, enemy|
+        # Choose the lowest-hp target, with reading-order-position as the tiebreaker
+        {enemy.hp, coords[1], coords[0]}
+      end.map {|coords, enemy| enemy}
+	end
+
 end
-
-class Elf < Warrior
-
-  def hates?(other : GridElement)
-    other.is_a?(Goblin)
-  end
-
-  def to_char
-    'E'
-  end
-end
-
-class Goblin < Warrior
-  def hates?(other : GridElement)
-    other.is_a?(Elf)
-  end
-
-  def to_char
-    'G'
-  end
-end
-
-class EmptySpace < GridElement
-  def tick(grid : Grid, own_coords : Coords)
-    # no-op, empty spaces don't do anything
-  end
-
-  def to_char
-    '.'
-  end
-end
-
-class Wall < GridElement
-  def tick(grid : Grid, own_coords : Coords)
-    # no-op, empty spaces don't do anything
-  end
-
-  def to_char
-    '#'
-  end
-end
-
-alias X = Int32
-alias Y = Int32
-alias Coords = Tuple(X, Y)
-
-
 class Grid
   def initialize(@arr : Array(Array(GridElement)))
   end
@@ -151,11 +169,12 @@ class Grid
 
   def neighbors(coords : Coords) : Array(Tuple(Coords, GridElement))
     x, y = coords
+    # Return neighbors in "reading order": up, left, right, down
     return [
-      {x + 1, y}, # 1 cell right
-      {x - 1, y}, # 1 cell left
-      {x, y + 1}, # 1 cell down
       {x, y - 1}, # 1 cell up
+      {x - 1, y}, # 1 cell left
+      {x + 1, y}, # 1 cell right
+      {x, y + 1}, # 1 cell down
     ].select {|coords| self.is_in_bounds?(coords)}
       .map {|x, y| { {x, y}, self.[x,y] } }
   end
@@ -165,49 +184,59 @@ class Grid
     b_x, b_y = b
     success = ->(c : Coords) { c == b }
     find_neighbors = ->(c : Coords) do
-      self.neighbors(c).reject {|coords, elem| elem.is_a?(Wall)}.map {|coords, elem| coords}
+      self.neighbors(c).select {|coords, elem| elem.is_a?(EmptySpace)}.map {|coords, elem| coords}
     end
     return Pathfinding.bfs(a, success, find_neighbors)
   end
 
-  def location_of(e : GridElement) : Coords?
-    @arr.each_with_index do |row, y|
-      row.each_with_index do |cell, x|
-        return {x, y} if cell == e
-      end
+  def tick
+    warriors_who_need_to_move = self.living_warriors
+    warriors_who_need_to_move.each do |warrior, coords|
+      next unless warrior.is_alive? # The warrior might have died before we "got to them"
+      x, y = coords
+      new_location = warrior.tick(self, coords)
+      # They're moving, so put an empty space where they're moving from
+      self[x, y] = EmptySpace.new
+			# then move them to their destination (which may be the same as their
+			# current location).
+			new_x, new_y = new_location
+			self[new_x, new_y] = warrior
     end
 
-    return nil
+    # Do a "final sweep" to sweep away dead warriors from the battlefield
+    warriors_who_need_to_move.reject {|warrior, coords| warrior.is_alive?}
+      .each {|dead_warrior, coords| self[coords[0], coords[1]] = EmptySpace.new}
   end
 
-  def tick
+  def living_warriors : Array(Tuple(Warrior, Coords))
+    warriors = [] of Tuple(Warrior, Coords)
+
     # "Turn order" happens from top to bottom, left to right
     @arr.each_with_index do |row, y|
       row.each_with_index do |cell, x|
-        cell.tick(self, {x, y})
-        if cell.is_a?(Warrior) && !cell.is_alive?
-          self[x, y] = EmptySpace.new
+        if cell.is_a?(Warrior)
+          warriors << {cell.as(Warrior), {x, y}}
         end
       end
     end
-  end
 
-  def living_warriors
-    @arr.flat_map do |row|
-      row.select(&.is_a?(Warrior)).map(&.as(Warrior))
-    end
+    return warriors
   end
 
   def winning_side
     cells = self.cells
-    there_are_still_elves = cells.any?(&.is_a?(Elf))
-    there_are_still_goblins = cells.any?(&.is_a?(Goblin))
+    there_are_still_elves = cells.any? do |coords, cell|
+			cell.is_a?(Warrior) && cell.as(Warrior).is_alive? && cell.as(Warrior).team.elf?
+		end
+    there_are_still_goblins = cells.any? do |coords, cell|
+			cell.is_a?(Warrior) && cell.as(Warrior).is_alive? && cell.as(Warrior).team.goblin?
+		end
 
-    case {there_are_still_elves, there_are_still_goblins}
-    when {true, false} then return Elf
-    when {false, true} then return Goblin
-    else return nil
-    end
+		case {there_are_still_elves, there_are_still_goblins}
+		when {true, false} then Warrior::Team::Elf
+		when {false, true} then Warrior::Team::Goblin
+		else nil
+		end
   end
 
   private def is_in_bounds?(x_y : Coords)
@@ -218,9 +247,18 @@ class Grid
     return true
   end
 
-  def to_s : String
+  def score : Int32?
+    self.living_warriors.map {|warrior, coords| warrior.hp}.sum
+  end
+
+  def to_s(include_health : Bool = false)  : String
     @arr.map do |row|
-      row.map(&.to_char).join("")
+      row_str = row.map(&.to_char).join("")
+      if include_health
+        warrior_healths = row.select(&.is_a?(Warrior)).map(&.as(Warrior).to_s).join(", ")
+        row_str += " #{warrior_healths}" unless warrior_healths.empty?
+      end
+      row_str
     end.join("\n")
   end
 
@@ -230,8 +268,8 @@ class Grid
         case cell
         when '#' then Wall.new
         when '.' then EmptySpace.new
-        when 'G' then Goblin.new
-        when 'E' then Elf.new
+        when 'G' then Warrior.new(Warrior::Team::Goblin)
+        when 'E' then Warrior.new(Warrior::Team::Elf)
         else raise "Unrecognized character: '#{cell}'"
         end.not_nil!
       end
@@ -242,27 +280,39 @@ end
 
 class Day15
   @grid : Grid
+	getter grid
 
   def initialize(input_lines : Array(String))
     @grid = Grid.parse(input_lines)
   end
 
-  def part_a
+  def part_a(run_sim : Bool = false)
     round_count = 0
+		redraw_sim if run_sim
     loop do
-      @grid.tick
-      winner = @grid.winning_side
-      if winner
-        total_hp_left = @grid.living_warriors.map(&.hp).sum
-        return round_count * total_hp_left
+      begin
+        @grid.tick
+      rescue Warrior::BattleIsOver
+        winner = @grid.winning_side
+        if winner
+          puts "Winner found on round #{round_count}"
+          total_hp_left = @grid.living_warriors.map(&.[0].hp).sum
+          return (round_count) * total_hp_left
+        end
       end
+			redraw_sim if run_sim
 
       round_count += 1
     end
   end
+
+	private def redraw_sim
+    puts "\n\n"
+		puts @grid.to_s(true)
+	end
 end
 
 unless PROGRAM_NAME.includes?("crystal-run-spec")
   day15 = Day15.new(File.read_lines("input.txt"))
-  puts "15A: #{day15.part_a}"
+  puts "15A: #{day15.part_a(true)}"
 end
